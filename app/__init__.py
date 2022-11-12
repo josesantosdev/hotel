2940d84cf1da2b7dec6492f114e07049a6814fb3
casfalking <casfalking@pop-os.localdomain>
2022-11-12 01:49:40 -0300

    feat quarto model and controllers, migrations and functional tests

--- app/__init__.py
+++ app/__init__.py
@@ -21,14 +21,17 @@ def create_app():
     # Models
     
     from app.models.hospede_model import Hospede
+    from app.models.quarto_model import Quarto
    
     # Registro das blueprints
     
     from app.controllers.hospede_contoller import HospedeController
     from app.controllers.doc_controller import DocumentationControoler
+    from app.controllers.quarto_controller import QuartoController
 
     app.register_blueprint(HospedeController.hospede_controller, url_prefix='/api/v1')
     app.register_blueprint(DocumentationControoler.documentation_controller, url_prefix='/api/v1')
+    app.register_blueprint(QuartoController.quarto_controller, url_prefix='/api/v1')
 
 
     return app
--- app/models/quarto_model.py
+++ app/models/quarto_model.py
@@ -1,52 +1,45 @@
 from app import db, ma
-from sqlalchemy import Column, BigInteger, String, Float, Boolean
+from sqlalchemy import Column, BigInteger, String, Float
 from marshmallow import fields
 
 
-class Quarto(db.model):
+class Quarto(db.Model):
+    __tablename__ = 'Quarto'
     id_quarto = Column(BigInteger().with_variant(db.Integer, dialect_name="sqlite"), primary_key=True)
     numero = Column(BigInteger)
     tipo = Column(String)
     preco = Column(Float)
     custo = Column(Float)
-    
-    
-    
-    def __init__(self, numero, tipo, preco, custo, disponibilidade):
+
+    def __init__(self, numero, tipo, preco, custo):
         self.numero = numero
         self.tipo = tipo
         self.preco = preco
         self.custo = custo
-        
-      
+
     def salvar(self):
         db.session.add(self)
         db.session.commit()
-    
+        return self
+
     def __repr__(self) -> str:
         return f'<id_quarto: {self.id_quarto}>'
-    
 
-  
+
 class QuartoSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Quarto
         load_instance = True
         sqla_session = db.session
-        
-        id_quarto = fields.Int()
-        numero = fields.Int()
+
+        id_quarto = fields.Integer(dump_only=True)
+        numero = fields.Integer()
         tipo = fields.Str()
         preco = fields.Float()
         custo = fields.Float()
 
-        
-    '''
-     _links = ma.Hyperlinks({
-        "colletion": ma.URLFor("hospede_controller.consultar_hospede"),
-        "self": ma.URLFor("hospede_controller.consultar_hospede_id", values=dict(id_hospede="<id_hospede>"))
+    
+    _links = ma.Hyperlinks({
+        "colletion": ma.URLFor("quarto_controller.consultar_quarto"),
+        "self": ma.URLFor("quarto_controller.consultar_quarto_id", values=dict(id_quarto="<id_quarto>"))
     })
-  
-    '''
-
-
