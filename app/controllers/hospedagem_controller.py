b597fa49923dc1b820a7eaa6265588056f093ad8
casfalking <casfalking@pop-os.localdomain>
2022-11-16 01:26:14 -0300

    feat hospedagem controller crud methods

---
+++ app/controllers/hospedagem_controller.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, request, json, Response
+
+from app.models.hospedagem_model import Hospedagem, HospedagemSchema
+
+
+from app import db
+
+
+class HospedagemController(object):
+
+    hospedagem_controller = Blueprint('hospedagem_controller', __name__)
+
+    @hospedagem_controller.route('/hospedagem/cadastrar', methods=['POST'])
+    def cadastrar_hospedagem():
+        request_data = request.get_json()
+        hospedagem_schema = HospedagemSchema()
+        hospedagem = hospedagem_schema.load(request_data)
+        return custom_response(hospedagem_schema.dump(hospedagem.salvar()), 201)
+
+    @hospedagem_controller.route('/hospedagem/consultar', methods=['GET'])
+    def consultar_hospedagem():
+        hospedagem = Hospedagem.query.all()
+        hospedagem_schema = HospedagemSchema(many=True)
+        return Response(hospedagem_schema.dump(hospedagem), 200)
+
+    @hospedagem_controller.route('/hospedagem/consultar/<id_hospedagem>', methods=['GET'])
+    def consultar_hospedagem_id(id_hospedagem):
+        hospedagem_por_id = Hospedagem.query.filter_by(id_hospedagem=id_hospedagem)
+        hospedagem_schema = HospedagemSchema(many=True)
+        return Response(hospedagem_schema.dump(hospedagem_por_id), 200)
+
+    @hospedagem_controller.route('/hospedagem/atualizar/<id_hospedagem>', methods=['PUT'])
+    def atualiazar_hospedagem(id_hospedagem):
+        hospede_schema = HospedagemSchema(partial=False)
+        query = Hospedagem.query.filter(Hospedagem.id_hospedagem == id_hospedagem)
+        query.update(request.get_json())
+        return custom_response(hospede_schema.dump(query.fisrt()), 201)
+
+    @hospedagem_controller.route('/hospedagem/deletar/<id_hospedagem>', methods=['DELETE'])
+    def deletar_hospedagem(id_hospedagem):
+        hospedagem = Hospedagem.query.filter(Hospedagem.id_hospedagem == id_hospedagem)
+        hospedagem.delete()
+        db.session.commit()
+        return custom_response({'Deletado':  f'id_hospede == {id_hospedagem}'}, 201)
+
+
+def custom_response(res, status_code):
+    return Response(
+        mimetype='application/json',
+        reponse=json.dhumps(res),
+        status=status_code
+
+    )
