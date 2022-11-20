5299d4bc98123f49d0a7d8854c199a9f3fed19eb
casfalking <thecaasantos@gmail.com>
2022-11-20 04:38:34 -0300

    feat create hospedagem rule with reserva hospedagem

--- app/models/reserva_hospedagem_model.py
+++ app/models/reserva_hospedagem_model.py
@@ -4,19 +4,32 @@ from sqlalchemy import Column, BigInteger, String, Date
 
 from app.models.quarto_model import Quarto
 
+from marshmallow import fields
+
+import datetime
+
 
 class Reserva(db.Model):
     __tablename__ = 'Reserva'
     id_reserva = Column(BigInteger().with_variant(db.Integer, dialect_name="sqlite"), primary_key=True)
     checkin = Column(Date, nullable=False)
     quarto = Column(BigInteger)
-    hospede_cpf = Column(String)
 
-    def __init__(self, id_reserva, checkin, quarto, hospede_cpf):
-        self.id_reserva = id_reserva
+    def __init__(self, checkin, quarto):
         self.checkin = checkin
-        self.quarto = Quarto.quarto_por_id(quarto)
-        self.hospede_cpf = hospede_cpf
+        self.quarto = quarto
+        
+        if datetime.date.today() == checkin:
+            quarto = Quarto.quarto_por_id(id_quarto=quarto)
+            quarto.set_disponibilidade(False)
+        
+    def consultar_reserva(self, checkin):
+        reserva = db.session.query(Reserva).filter(Reserva.checkin == checkin)
+        return reserva.disponibilidade
+    
+    @staticmethod
+    def reserva_por_checkin(id_reserva):
+        return Reserva.query.filter(Reserva.id_reserva==id_reserva).first()
 
     def salvar(self):
         db.session.add(self)
@@ -30,6 +43,10 @@ class ReservaSchema(ma.SQLAlchemyAutoSchema):
         model = Reserva
         load_instance = True
         sqla_session = db.session
+        
+        id_reserva = fields.Integer(dump_only=True)
+        quarto = fields.Integer()
+        disponibilidade = fields.Boolean(load_only=True)
 
     _links = ma.Hyperlinks({
         "colletion": ma.URLFor("reserva_controller.consultar_reserva"),
